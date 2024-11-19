bb713b2ec2a86f993e166724932b0c37f366dc5d
FedeCoco13 <fed.toni@gmail.com>
2024-11-19 06:08:19 +0100

    fixmobile49

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -6,8 +6,11 @@ export default function Document() {
       <Head>
         <meta
           name="viewport"
-          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=0"
+          content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no"
         />
+        <meta name="format-detection" content="telephone=no" />
+        <meta name="mobile-web-app-capable" content="yes" />
+        <meta name="apple-mobile-web-app-capable" content="yes" />
       </Head>
       <body>
         <Main />
